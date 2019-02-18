afc147ad03ba278f8b67afa224c4c67e39489f81
MenThu <422729946@qq.com>
2019-02-18 11:20:55 +0800

    Test

---
+++ TestCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCocoaPods
+//
+//  Created by MenThu on 2019/2/18.
+//  Copyright © 2019 MenThu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCocoaPods
+//
+//  Created by MenThu on 2019/2/18.
+//  Copyright © 2019 MenThu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
