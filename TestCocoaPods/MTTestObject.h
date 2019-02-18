de8f8ff35a211ca012bc9ab0da7dae63314a9ece
MenThu <422729946@qq.com>
2019-02-18 11:28:39 +0800

    Add - 新增工具类

---
+++ TestCocoaPods/MTTestObject.h
@@ -0,0 +1,15 @@
+//
+//  MTTestObject.h
+//  TestCocoaPods
+//
+//  Created by MenThu on 2019/2/18.
+//  Copyright © 2019 MenThu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTTestObject : NSObject
+
++ (void)testMethod;
+
+@end
---
+++ TestCocoaPods/MTTestObject.m
@@ -0,0 +1,17 @@
+//
+//  MTTestObject.m
+//  TestCocoaPods
+//
+//  Created by MenThu on 2019/2/18.
+//  Copyright © 2019 MenThu. All rights reserved.
+//
+
+#import "MTTestObject.h"
+
+@implementation MTTestObject
+
++ (void)testMethod{
+    NSLog(@"TestCocoaPodMethod:[%s:%d]", __FILE__, __LINE__);
+}
+
+@end
